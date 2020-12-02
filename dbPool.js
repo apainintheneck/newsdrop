e37fb2b20f550ae6dab5746323c3f0aaa9a5fec0
apainintheneck <apainintheneck@gmail.com>
2020-12-02 07:18:44 +0000

    Added database info to the dbPool.js file.

--- dbPool.js
+++ dbPool.js
@@ -1,11 +1,11 @@
-// const mysql = require('mysql');
+const mysql = require('mysql');
 
-// const pool = mysql.createPool({ 
-//     connectionLimit: 10,
-//     host: hostname,
-//     user: username,
-//     password: password,
-//     database: database_name
-// });
+const pool = mysql.createPool({ 
+    connectionLimit: 10,
+    host: "de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "f1m9vwrbgen0ox6j",
+    password: "mbs198qklr60b6n7",
+    database: "cslmbxgxzt3gjvvx"
+});
 
-// module.exports = pool;
+module.exports = pool;
