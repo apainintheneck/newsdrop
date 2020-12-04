856b8b627368e9e97cb1232254a79aa848ba354b
apainintheneck <apainintheneck@gmail.com>
2020-12-04 00:49:00 +0000

    Added the ability to add to posts table of db but the db is not connecting so I can't test it.

--- app.js
+++ app.js
@@ -21,7 +21,20 @@ app.get("/add", function(req, res){
 
 app.post("/added", function(req, res){
     console.log(req.body); //For testing.
+    
+    let sql;
+    let sqlParams;
+    
+    sql = "INSERT INTO posts (title, type, url, description) VALUES (?,?,?,?)";
+    sqlParams = [req.body.title, req.body.type, req.body.url, req.body.description];
+    
+    pool.query(sql, sqlParams, function (err, rows, fields){
+        if (err) throw err;
+        console.log(rows); 
+    });
+
     res.render("added");
+    
 });
 
 //starting server
