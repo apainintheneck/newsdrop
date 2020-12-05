a2e43a5d928aa93224d250f45f8e1e73291438d1
apainintheneck <apainintheneck@gmail.com>
2020-12-05 19:35:39 +0000

    Finished form submission part of the final project. Submits posts and tags to db. Also, added comments.

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,18 +1,4 @@
 /* global $ */
 $(document).ready(function(){
-    function addTags(insertID, tags){
-        let sql = "INSERT INTO posts (post_ID, tag) VALUES";
-        let sqlParams = [];
-        
-        for(let i = 0; i < tags.length; i++){
-            sql = sql.concat(" (?,?)");
-            if(i + 1 === tags.length) sql = sql.concat(",");
-            sqlParams.push(insertID, tags[i]);
-        }
-        
-        sql = sql.concat(";");
-        
-        console.log(sql);
-        console.log(sqlParams);
-    }
+    
 });
