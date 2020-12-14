53aaf694a6aeba29944cb99f378c0337469bfe67
EC2 Default User <ec2-user@ip-172-31-68-222.ec2.internal>
2020-12-14 23:16:10 +0000

    New changes with an error

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,4 +1,4 @@
-//import reddit from './redditapi';
+import reddit from 'js/redditapi';
 
 const searchForm = document.getElementById('search-form');
 const searchBtn = document.getElementById('search-btn');
@@ -20,32 +20,32 @@ searchForm.addEventListener('submit', e => {
   searchInput.value = '';
 
   // Search Reddit
-  // reddit.search(searchTerm, searchLimit, sortBy).then(results => {
-  //   let output = '<div class="card-columns">';
-  //   console.log(results);
-  //   results.forEach(post => {
-  //     // Check for image
-  //     let image = post.preview
-  //       ? post.preview.images[0].source.url
-  //       : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
-  //     output += `
-  //     <div class="card mb-2">
-  //     <img class="card-img-top" src="${image}" alt="Card image cap">
-  //     <div class="card-body">
-  //       <h5 class="card-title">${post.title}</h5>
-  //       <p class="card-text">${truncateString(post.selftext, 100)}</p>
-  //       <a href="${post.url}" target="_blank
-  //       " class="btn btn-primary">Read More</a>
-  //       <hr>
-  //       <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span> 
-  //       <span class="badge badge-dark">Score: ${post.score}</span>
-  //     </div>
-  //   </div>
-  //     `;
-  //   });
-  //   output += '</div>';
-  //   document.getElementById('results').innerHTML = output;
-  // });
+  reddit.search(searchTerm, searchLimit, sortBy).then(results => {
+    let output = '<div class="card-columns">';
+    console.log(results);
+    results.forEach(post => {
+      // Check for image
+      let image = post.preview
+        ? post.preview.images[0].source.url
+        : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
+      output += `
+      <div class="card mb-2">
+      <img class="card-img-top" src="${image}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${post.title}</h5>
+        <p class="card-text">${truncateString(post.selftext, 100)}</p>
+        <a href="${post.url}" target="_blank
+        " class="btn btn-primary">Read More</a>
+        <hr>
+        <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span> 
+        <span class="badge badge-dark">Score: ${post.score}</span>
+      </div>
+    </div>
+      `;
+    });
+    output += '</div>';
+    document.getElementById('results').innerHTML = output;
+  });
 
   e.preventDefault();
 });
