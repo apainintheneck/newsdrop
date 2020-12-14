83b6e8b5dbbb25bda3c6ebaa562ae63571268e14
brycerandolph <brycerandolph75@gmail.com>
2020-12-14 02:15:30 +0000

    userlinks page

--- app.js
+++ app.js
@@ -11,6 +11,22 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
+
+app.get("/userlinks", function(req, res){
+    res.render("userlinks");
+});
+
+app.get("/api/getUserFavorites", function(req, res){
+  let sql = "SELECT * FROM posts";
+  pool.query(sql, function (err, rows, fields) {
+    
+    if (err) throw err;
+    console.log(rows);
+    res.send(rows);
+  });
+    
+});
+
 //starting server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Express server is running..."); 
